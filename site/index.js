744b6a1f53ff54f43b65a9d41ee985b888da42f8
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-15 16:51:18 +0200

    initial commit

---
+++ site/index.js
@@ -0,0 +1,25 @@
+import { fetchAndShowTweets } from "./Tweets.bs.js"
+import { set as setStatus } from "./Status.bs.js"
+import { mark } from "./Mark.bs.js"
+
+setStatus("fauna GET")
+fetch(`/.netlify/functions/fauna`).then(faunaResp => {
+  if (!faunaResp.ok) {
+    faunaResp.text().then(text => {
+      setStatus(`fauna GET error: ${text}`)
+    })
+  } else
+    faunaResp.json().then(({ id_str }) => {
+      setStatus("twitter GET")
+      fetchAndShowTweets(id_str, document.getElementById("tweets")).then(
+        () => {
+          console.log("done")
+        },
+        () => {
+          console.log("fail")
+        }
+      )
+    })
+})
+
+window.mark = mark
