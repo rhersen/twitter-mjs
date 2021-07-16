565b172b3a044a22d13b4984b17979b484c38fc9
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-16 13:56:13 +0200

    don't declare functions with "const"

--- site/fetchAndShowTweets.js
+++ site/fetchAndShowTweets.js
@@ -4,15 +4,18 @@ import setStatus from "./setStatus.js";
 
 export default async (id_str, tweets) => {
   const since = (s) => fetch(`/.netlify/functions/twitter?since_id=${s}`);
-  const handleJson = function (tweetJson) {
+
+  function handleJson(tweetJson) {
     const users = getUsers(tweetJson);
-    const renderTweets = function (tweet, i) {
+
+    function renderTweets(tweet, i) {
       tweets.insertAdjacentHTML("afterbegin", renderTweet(tweet));
       return tweets.insertAdjacentHTML(
         "afterbegin",
         `<div class="stats"><span class="countdown" onclick='mark("${tweet.id_str}")'>${i}</span><hr /></div>`
       );
-    };
+    }
+
     tweetJson.forEach(renderTweets);
     tweets.insertAdjacentHTML(
       "afterbegin",
@@ -28,16 +31,18 @@ export default async (id_str, tweets) => {
         .join("")}</table>`
     );
     return Promise.resolve(setStatus("twitter GET OK"));
-  };
-  const handleFetch = async function (tweetResp) {
+  }
+
+  async function handleFetch(tweetResp) {
     if (tweetResp.ok) {
       setStatus("insertAdjacentHTML");
       const tweetJson = await tweetResp.json();
       return handleJson(tweetJson);
     }
     const s = await tweetResp.text();
-    setStatus(`twitter GET error: ${s}`)
-  };
+    setStatus(`twitter GET error: ${s}`);
+  }
+
   const tweetResp = await since(id_str);
   return handleFetch(tweetResp);
 };
--- site/renderTweet.js
+++ site/renderTweet.js
@@ -1,20 +1,22 @@
 export default (tweet) => {
-  const getImages = function (d) {
-    const isPhoto = function (img) {
+  function getImages(d) {
+    function isPhoto(img) {
       if (img.type === "photo" || img.type === "video") {
         return true;
       } else {
         return img.type === "animated_gif";
       }
-    };
-    const maxBitrate = function (prev, cur) {
+    }
+
+    function maxBitrate(prev, cur) {
       if (cur.bitrate > prev.bitrate) {
         return cur;
       } else {
         return prev;
       }
-    };
-    const getVideoLink = function (info) {
+    }
+
+    function getVideoLink(info) {
       const variants = info !== undefined ? info.variants : [];
       if (variants.length === 0) {
         return "";
@@ -32,8 +34,9 @@ export default (tweet) => {
       }
       const duration = `${millis} ms`;
       return `<a href="${best.url}">${duration}</a>`;
-    };
-    const getImage = function (image) {
+    }
+
+    function getImage(image) {
       const size = image.sizes.small;
       const width = size.w / 2;
       const height = size.h / 2;
@@ -42,49 +45,57 @@ export default (tweet) => {
       const img = `<img src="${small}" width="${width}" height="${height}" />`;
       const duration = getVideoLink(image.video_info);
       return `<a href="${large}">${img}</a>${duration}`;
-    };
+    }
+
     const match = d.extended_entities;
     if (match !== undefined) {
       return match.media.filter(isPhoto).map(getImage).join("");
     } else {
       return "";
     }
-  };
-  const getUser = function (retweet, d) {
+  }
+
+  function getUser(retweet, d) {
     if (retweet !== undefined) {
       return retweet.user.screen_name;
     } else {
       return d.user.screen_name;
     }
-  };
-  const getRetweeter = function (retweet, d) {
+  }
+
+  function getRetweeter(retweet, d) {
     if (retweet !== undefined) {
       return ` <i>${d.user.screen_name}</i> `;
     } else {
       return " ";
     }
-  };
-  const fullText = function (data) {
+  }
+
+  function fullText(data) {
     return data.full_text.replace(/\n/g, "<br>");
-  };
-  const getText = function (retweetStatus, tweetStatus) {
-    const replaceUrlWithLink = function (text, url) {
+  }
+
+  function getText(retweetStatus, tweetStatus) {
+    function replaceUrlWithLink(text, url) {
       return text.replace(
         url.url,
         `<a href="${url.url}" target="_blank">${url.display_url}</a>`
       );
-    };
+    }
+
     const data = retweetStatus !== undefined ? retweetStatus : tweetStatus;
     return data.entities.urls.reduce(replaceUrlWithLink, fullText(data));
-  };
-  const getQuote = function (d) {
+  }
+
+  function getQuote(d) {
     const quotedStatus = d.quoted_status;
     if (quotedStatus !== undefined) {
       return `<div class="quoted">${fullText(quotedStatus)}</div>`;
     } else {
       return "";
     }
-  };
+  }
+
   const retweet = tweet.retweeted_status;
   const time = tweet.created_at.substr(8, 8);
   const user = getUser(retweet, tweet);
