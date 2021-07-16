13a919994c63eb28aad9669b2ca75e0966f5b97f
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-16 14:15:30 +0200

    top-level await

--- site/index.js
+++ site/index.js
@@ -3,20 +3,19 @@ import setStatus from "./setStatus.js";
 import mark from "./mark.js";
 
 setStatus("fauna GET");
-fetch(`/.netlify/functions/fauna`).then(async (faunaResp) => {
-  if (!faunaResp.ok) {
-    const text = await faunaResp.text();
-    setStatus(`fauna GET error: ${text}`);
-  } else {
-    const { id_str } = await faunaResp.json();
-    setStatus("twitter GET");
-    try {
-      await fetchAndShowTweets(id_str, document.getElementById("tweets"));
-      console.log("done");
-    } catch (e) {
-      console.log("fail");
-    }
+const faunaResp = await fetch(`/.netlify/functions/fauna`);
+if (!faunaResp.ok) {
+  const text = await faunaResp.text();
+  setStatus(`fauna GET error: ${text}`);
+} else {
+  const { id_str } = await faunaResp.json();
+  setStatus("twitter GET");
+  try {
+    await fetchAndShowTweets(id_str, document.getElementById("tweets"));
+    console.log("done");
+  } catch (e) {
+    console.log("fail");
   }
-});
+}
 
 window.mark = mark;
