8ad0c1db34d57363197285e28dc0fa0bca58db0c
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-07-16 14:23:18 +0200

    put functions below code
    (cherry picked from commit 65f2f663438e81f1e969c31a52c6564c350d7755)

--- site/renderTweet.js
+++ site/renderTweet.js
@@ -1,5 +1,25 @@
 export default (tweet) => {
+  const retweet = tweet.retweeted_status;
+  const time = tweet.created_at.substr(8, 8);
+  const user = getUser(retweet, tweet);
+  const t = retweet !== undefined ? retweet : tweet;
+  const image = getImages(t);
+  const a = `<a href="https://twitter.com/${user}/status/${tweet.id_str}""" target="_blank">${time}</a>`;
+  const i = getRetweeter(retweet, tweet);
+  const b = `<b>${user}</b>`;
+  const text = getText(retweet, tweet);
+  const images = `<div>${image}</div>`;
+  const quote = getQuote(t);
+  return `<li>${a}${i}${b} ${text} ${quote} ${images}</li>`;
+
   function getImages(d) {
+    const match = d.extended_entities;
+    if (match !== undefined) {
+      return match.media.filter(isPhoto).map(getImage).join("");
+    } else {
+      return "";
+    }
+
     function isPhoto(img) {
       if (img.type === "photo" || img.type === "video") {
         return true;
@@ -46,13 +66,6 @@ export default (tweet) => {
       const duration = getVideoLink(image.video_info);
       return `<a href="${large}">${img}</a>${duration}`;
     }
-
-    const match = d.extended_entities;
-    if (match !== undefined) {
-      return match.media.filter(isPhoto).map(getImage).join("");
-    } else {
-      return "";
-    }
   }
 
   function getUser(retweet, d) {
@@ -76,15 +89,15 @@ export default (tweet) => {
   }
 
   function getText(retweetStatus, tweetStatus) {
+    const data = retweetStatus !== undefined ? retweetStatus : tweetStatus;
+    return data.entities.urls.reduce(replaceUrlWithLink, fullText(data));
+
     function replaceUrlWithLink(text, url) {
       return text.replace(
         url.url,
         `<a href="${url.url}" target="_blank">${url.display_url}</a>`
       );
     }
-
-    const data = retweetStatus !== undefined ? retweetStatus : tweetStatus;
-    return data.entities.urls.reduce(replaceUrlWithLink, fullText(data));
   }
 
   function getQuote(d) {
@@ -95,17 +108,4 @@ export default (tweet) => {
       return "";
     }
   }
-
-  const retweet = tweet.retweeted_status;
-  const time = tweet.created_at.substr(8, 8);
-  const user = getUser(retweet, tweet);
-  const t = retweet !== undefined ? retweet : tweet;
-  const image = getImages(t);
-  const a = `<a href="https://twitter.com/${user}/status/${tweet.id_str}""" target="_blank">${time}</a>`;
-  const i = getRetweeter(retweet, tweet);
-  const b = `<b>${user}</b>`;
-  const text = getText(retweet, tweet);
-  const images = `<div>${image}</div>`;
-  const quote = getQuote(t);
-  return `<li>${a}${i}${b} ${text} ${quote} ${images}</li>`;
 };
